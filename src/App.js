6bc01ba4aa3181d74d90bc57791d6a6f1cc9a990
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-01-21 18:23:45 -0500

    Created An Entry Form With Functionality That Allows The User To Submit New Entries.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: "1-21-2023"}, {weight: 176, date: "1-22-2023"}])
 
+  function AddNewEntry(entry) {
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries);
+  }
+
   return (
     <div >
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryProp={AddNewEntry}/>
     </div>
   );
 }
